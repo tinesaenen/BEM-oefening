505be6116b7ef8f8d962582ee4e3e63220a36c4f
tine <tinesaenen@hotmail.be>
2020-01-03 17:02:13 +0100

    jquery toggle en extra paragraaf toegevoegd

---
+++ script.js
@@ -0,0 +1,7 @@
+$(".image__button--pink").click(function() {
+  $(".image__hiddenSmileyHappy").toggle();
+});
+
+$(".image__button--darkblue").click(function() {
+  $(".image__hiddenSmileySad").toggle();
+});
